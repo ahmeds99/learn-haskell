7cbbded8bd9a3e23705e6edb5307d422f5f742f9
Ahmed Waseem Saeed <ahmedws@uio.no>
2023-08-05 21:07:50 +0200

    add ex.3, could not complete

--- cis194/hw3/Golf.hs
+++ cis194/hw3/Golf.hs
@@ -1,4 +1,7 @@
 module Golf where
+import Data.List
+import Control.Arrow((&&&))
+import Data.Function (on)
 
 -- Ex. 1: Hopscotch
 -- Make sure every element is the same as the first
@@ -15,7 +18,7 @@ nthElement (n, ls) = every n ls
 
 -- Gives us the index with the list, 1-indexed, to allow for every nth item to get extracted
 toIndexTuple :: [b] -> [(Int, b)]
-toIndexTuple = zip[1..]
+toIndexTuple = zip [1..]
 
 -- Maps everything to the first element, gets the indicies, and extracts only the nth elements
 skips :: [a] -> [[a]]
@@ -29,3 +32,8 @@ localMaxima (x:rest@(y:z:_))
     | y > x && y > z    = y : localMaxima rest
     | otherwise         = localMaxima rest
 localMaxima _ = []
+
+
+-- Ex. 3 Histogram
+-- Faied after some tries.
+-- Check this for solution,  https://github.com/bschwb/cis194-solutions/blob/main/03-rec-poly/Golf.hs
