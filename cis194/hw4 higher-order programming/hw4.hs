58ee2e9fb0160233e5a35729effc4eb0852a7343
Ahmed Waseem Saeed <ahmedws@uio.no>
2023-08-06 17:31:53 +0200

    finally complete ex. 2

--- cis194/hw4 higher-order programming/hw4.hs
+++ cis194/hw4 higher-order programming/hw4.hs
@@ -24,3 +24,39 @@ fun2' =
     . filter even
     . takeWhile (/= 1) 
     . iterate (\n -> if even n then n `div` 2 else 3 * n + 1)
+
+
+-- Ex. 2: Folding with trees
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+    deriving (Show, Eq)
+
+insertNode :: a -> Tree a -> Tree a
+insertNode a Leaf = Node 0 Leaf a Leaf
+insertNode a (Node h left x right)
+    | height1 < height2 = Node h                           (insertNode a left) x right
+    | height1 > height2 = Node h                           left x (insertNode a right)
+    | otherwise         = Node (1 + max (heightOfTree left) heightInserted) left x (insertNode a right)
+    where 
+        height1 = heightOfTree left
+        height2 = heightOfTree right
+        inserted = insertNode a right
+        heightInserted = heightOfTree inserted
+        
+
+heightOfTree :: Tree a -> Integer
+heightOfTree Leaf = -1
+heightOfTree (Node height _ _ _) = height
+
+foldTree :: [a] -> Tree a
+foldTree = foldr insertNode Leaf
+
+-- From: https://stackoverflow.com/questions/16157203/build-balanced-binary-tree-with-foldr
+showTree :: Show a => Tree a -> String
+showTree Leaf = ""  
+showTree n@(Node i _ _ _) = go i n
+  where
+  go _ Leaf = "" 
+  go i (Node _ l c r) = go (i-1) l ++ 
+    replicate (4*fromIntegral i) ' ' ++ show c ++ "\n" ++ go (i-1) r 
+
