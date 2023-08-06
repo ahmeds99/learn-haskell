3c1a38ea13b8f55d8afee77eb26df29eabb7c2b3
Ahmed Waseem Saeed <ahmedws@uio.no>
2023-08-06 15:39:21 +0200

    ex 1 completed, after some hints for the second part

--- cis194/hw4 higher-order programming/hw4.hs
+++ cis194/hw4 higher-order programming/hw4.hs
@@ -14,3 +14,13 @@ fun2 1 = 0
 fun2 n | even n = n + fun2 (n `div` 2)
        | otherwise = fun2 (3 * n + 1)
 
+fun2' :: Integer -> Integer
+-- fun2' 1 = 0
+-- fun2' n = if even n then n + fun2 (n `div` 2) else fun2 (3 * n + 1)
+
+-- After looking at solution for hints
+fun2' = 
+      sum 
+    . filter even
+    . takeWhile (/= 1) 
+    . iterate (\n -> if even n then n `div` 2 else 3 * n + 1)
