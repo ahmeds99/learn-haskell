6290bfd48ee579f8c855ebf600fb2766cd120423
Ahmed Waseem Saeed <ahmedws@uio.no>
2023-08-06 09:20:46 +0200

    begin ex. 1 in hw4

---
+++ cis194/hw4 higher-order programming/hw4.hs
@@ -0,0 +1,16 @@
+-- Ex. 1: Rewrite fun1 and fun2 functions in a more idiomatic Haskell style
+-- Aka wholemeal programming
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+    | even x = (x - 2) * fun1 xs
+    | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = product . map (\x -> x - 2) . filter even
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
+
