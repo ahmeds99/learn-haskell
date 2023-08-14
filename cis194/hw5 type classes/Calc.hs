25fd43cbf02770703c818c2d84200ce6d5a22856
Ahmed Waseem Saeed <ahmedws@uio.no>
2023-08-15 00:48:34 +0200

    complete ex 2

--- cis194/hw5 type classes/Calc.hs
+++ cis194/hw5 type classes/Calc.hs
@@ -1,6 +1,7 @@
 module Calc where
 
 import ExprT
+import Parser
 
 -- Ex. 1
 eval :: ExprT -> Integer
@@ -8,3 +9,7 @@ eval :: ExprT -> Integer
 eval (Lit x) = x
 eval (Add a b) = eval a + eval b
 eval (Mul a b) = eval a * eval b
+
+-- Ex. 2
+evalStr :: String -> Maybe Integer
+evalStr s = eval <$> parseExp Lit Add Mul s
