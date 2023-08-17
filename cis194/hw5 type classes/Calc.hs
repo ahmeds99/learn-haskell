8723af5cd0df4e5ecb714c657c5bfb8443599cf0
Ahmed Waseem Saeed <ahmedws@uio.no>
2023-08-17 20:58:28 +0200

    complete ex. 6, with some help

--- cis194/hw5 type classes/Calc.hs
+++ cis194/hw5 type classes/Calc.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# OPTIONS_GHC -Wall -Werror #-}
 
 module Calc where
 
 import ExprT
 import Parser
+import qualified Data.Map as M
+import Data.Maybe (fromJust, isNothing)
 
 -- Ex. 1
 eval :: ExprT -> Integer
@@ -14,7 +17,7 @@ eval (Mul a b) = eval a * eval b
 
 -- Ex. 2
 evalStr :: String -> Maybe Integer
-evalStr s = eval <$> parseExp Lit Add Mul s
+evalStr s = eval <$> parseExp ExprT.Lit ExprT.Add ExprT.Mul s
 
 -- Ex. 3
 class Expr a where
@@ -23,7 +26,7 @@ class Expr a where
     mul :: a -> a -> a
 
 instance Expr ExprT where
-    lit  = Lit
+    lit = Lit
     add = Add
     mul = Mul
 
@@ -70,3 +73,40 @@ testMM = testExp :: Maybe MinMax
 
 testSat :: Maybe Mod7
 testSat = testExp :: Maybe Mod7
+
+-- Ex. 6
+class HasVars a where
+    var :: String -> a
+
+data VarExprT = VarExprT String Integer
+  deriving (Show, Eq)
+
+instance Expr VarExprT where
+    lit = VarExprT ""
+    add (VarExprT _ a) (VarExprT _ b) = VarExprT "" (a + b)
+    mul (VarExprT _ a) (VarExprT _ b) = VarExprT "" (a * b)
+
+instance HasVars VarExprT where
+    var s = VarExprT s 0
+
+instance HasVars (M.Map String Integer -> Maybe Integer) where
+    var = M.lookup
+
+-- Looked a bit on a solution for this unfortonuately, from here: https://github.com/bschwb/cis194-solutions/blob/main/05-typeclasses/Calc.hs
+instance Expr (M.Map String Integer -> Maybe Integer) where
+    lit i _ = Just i
+
+    add a b m = if isNothing (a m) || isNothing (b m)
+                then Nothing
+                else Just (fromJust (a m) + fromJust (b m))
+
+    mul a b m = if isNothing (a m) || isNothing (b m)
+                then Nothing
+                else Just (fromJust (a m) * fromJust (b m))
+
+-- From course page
+withVars :: [(String, Integer)]
+            -> (M.Map String Integer -> Maybe Integer)
+            -> Maybe Integer
+withVars vs e = e $ M.fromList vs
+
