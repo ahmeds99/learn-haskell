9a4f2954a5dc76f5178eb874bd1bb50e05a2ea38
Ahmed Waseem Saeed <ahmedws@uio.no>
2023-08-19 00:08:59 +0200

    begin streams exercises, ex. 3 done, not tested

--- cis194/hw6 lazy eval/Fibonacci.hs
+++ cis194/hw6 lazy eval/Fibonacci.hs
@@ -14,3 +14,14 @@ fibs1 = map fib [0..]
 fibs2 :: [Integer]
 -- https://wiki.haskell.org/The_Fibonacci_sequence#Canonical_zipWith_implementation
 fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
+
+-- Ex. 3: Streams
+data Stream x = Cons x (Stream x)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons a b) = a : streamToList b
+
+instance Show a => Show (Stream a) where
+    show a = show (take 20 (streamToList a))
+
+-- Ex. 4: more streams
