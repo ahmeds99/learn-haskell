388e4400581f386052e9472f8ab2901c01244c0e
Ahmed Waseem Saeed <ahmedws@uio.no>
2023-08-18 23:41:09 +0200

    ex 2 with help from docs

--- cis194/hw6 lazy eval/Fibonacci.hs
+++ cis194/hw6 lazy eval/Fibonacci.hs
@@ -9,3 +9,8 @@ fib n = fib (n - 1) + fib (n - 2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
+
+-- Ex. 2
+fibs2 :: [Integer]
+-- https://wiki.haskell.org/The_Fibonacci_sequence#Canonical_zipWith_implementation
+fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
